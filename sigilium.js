5a1c78f9cbd9869347f75898c39d55c874cf1778
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-23 08:33:08 -0700

    Updated by Phantomaton

---
+++ sigilium.js
@@ -0,0 +1,79 @@
+class Sigil {
+  constructor(name) {
+    this.name = name;
+    this.impl = Symbol(`${name}:impl`);
+    this.resolve = Symbol(`${name}:resolve`);
+  }
+
+  provider(dependencies, factory) {
+    return {
+      symbol: this.impl,
+      dependencies,
+      factory
+    };
+  }
+
+  resolver() {
+    return {
+      symbol: this.resolve,
+      dependencies: [this.impl],
+      factory: impls => impls[0]
+    };
+  }
+}
+
+class Singleton extends Sigil {
+  resolver() {
+    return {
+      symbol: this.resolve,
+      dependencies: [this.impl],
+      factory: impls => {
+        if (impls.length !== 1) {
+          throw new Error(`Expected exactly one implementation for ${this.name}`);
+        }
+        return impls[0];
+      }
+    };
+  }
+}
+
+class Composite extends Sigil {
+  constructor(name) {
+    super(name);
+    this.decorate = Symbol(`${name}:decorate`);
+    this.aggregate = Symbol(`${name}:aggregate`);
+  }
+
+  decorator(dependencies, factory) {
+    return {
+      symbol: this.decorate,
+      dependencies,
+      factory
+    };
+  }
+
+  aggregator(dependencies, factory) {
+    return {
+      symbol: this.aggregate,
+      dependencies,
+      factory
+    };
+  }
+
+  resolver() {
+    return {
+      symbol: this.resolve,
+      dependencies: [this.impl, this.decorate, this.aggregate],
+      factory: (impls, decorators = [], [aggregator]) => {
+        const base = aggregator ? aggregator(impls) : impls[0];
+        return decorators.reduce((fn, decorate) => decorate(fn), base);
+      }
+    };
+  }
+}
+
+export default {
+  sigil: name => new Sigil(name),
+  singleton: name => new Singleton(name),
+  composite: name => new Composite(name)
+};
